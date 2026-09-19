efc9396e9ce7baf00fda7607355527177d06be41
Lalitkishore2 <svlalitk2k@gmail.com>
2026-09-19 14:49:46 +0530

    style: replace green button styling with Aquadot ocean blue gradient

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Component Vault Service Worker - Offline PWA Cache
-const CACHE_NAME = 'component-vault-v1.3';
+const CACHE_NAME = 'component-vault-v1.4';
 const CORE_ASSETS = [
   './',
   './index.html',
