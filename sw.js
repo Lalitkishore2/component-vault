741e7b734f9a71810a7d7ca06bd3ae01b7dbf944
Lalitkishore2 <svlalitk2k@gmail.com>
2026-09-19 20:50:54 +0530

    feat(theme): dark mode pure black-icon metallic noir color theme

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Component Vault Service Worker - Offline PWA Cache
-const CACHE_NAME = 'component-vault-v2.7';
+const CACHE_NAME = 'component-vault-v2.8';
 const CORE_ASSETS = [
   './',
   './index.html',
